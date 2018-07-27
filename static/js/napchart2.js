808e81713c0c89a129132f3049d7c76a6678bc18
jackred <gouzoudorian@gmail.com>
2018-07-27 22:13:52 +0200

    ADD event on chart
    add e3 button to have an example
    event are lane adaptable
    add an array to keep track of outer/inner radius

--- static/js/napchart2.js
+++ static/js/napchart2.js
@@ -8,6 +8,9 @@ var ring = [],
     border_ring = [],
     extreme_ring = null;
 
+var event = [],
+    n_event = 0;
+
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
@@ -15,82 +18,68 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 
-var rings = svg.selectAll(".ring"),
-    border_rings = svg.selectAll(".border_ring"),
+var border_rings = svg.selectAll(".border_ring"),
     extreme_rings = svg.selectAll(".extreme_ring");
 
+var events = svg.selectAll(".events");
 
+
+/*==========*/
+
+function to_h(n){
+    return n * pi;
+}
+
+
+function h_n_m(h,m){
+    return (h + (m/60));
+}
 
 /*==========*/
 
 
 function set_extreme_ring(){
-    if (n_ring == 1){
-	extreme_ring = d3.svg.arc()
-	    .startAngle(0)
-	    .endAngle(pi)
-	    .innerRadius(radius * 0.6)
-	    .outerRadius(radius * 0.9);
-    }else if (n_ring != 0) {
-	extreme_ring = d3.svg.arc()
-	    .startAngle(0)
-	    .endAngle(pi)
-	    .innerRadius(radius * 0.3)
-	    .outerRadius(radius * 0.9)
-    }
+    extreme_ring = d3.svg.arc()
+	.startAngle(0)
+	.endAngle(pi)
+	.innerRadius(ring[0].inner)
+	.outerRadius(ring[n_ring-1].outer);
+
 }
 
 function set_border_ring(){
     border_ring.length = 0;
     if (n_ring != 1){
 	for (var i = 0 ; i < n_ring ; i++){
-	    let delta = 0.6 / n_ring
-	    let arc = d3.svg.arc()
-		.startAngle(0)
-		.endAngle(pi)
-		.innerRadius(radius * (0.3 + (i * delta)))
-		.outerRadius(radius * (0.3 + ((i+1) * delta)));
-	    border_ring.push(arc)
+    	    let arc = d3.svg.arc()
+    		.startAngle(0)
+    		.endAngle(pi)
+    		.innerRadius(ring[i].inner)
+    		.outerRadius(ring[i].outer);
+    	    border_ring.push(arc)
+    	}
+    }
+}
+
+function set_ring(start, endAngle){
+    ring.length = 0;
+    if (n_ring == 1){
+	let inner = (radius * 0.6);
+	let outer = (radius * 0.9);
+	ring.push({"inner": inner, "outer": outer});
+    }else if (n_ring != 0) {
+	for (var i = 0 ; i < n_ring ; i++){
+	    let delta = 0.6 / n_ring;
+	    let inner = (radius * (0.3 + (i * delta)));
+	    let outer = (radius * (0.3 + ((i+1) * delta)));
+	    ring.push({"inner": inner, "outer": outer});
 	}
     }
 }
 
-function set_ring(){
-    /*     ring.length = 0;
-     *     if (n_ring == 1){
-     * 	let arc = d3.svg.arc()
-     * 		    .startAngle(0)
-     * 		    .endAngle(pi)
-     * 		    .innerRadius(radius * (0.6 + 0.015))
-     * 		    .outerRadius(radius * (0.9 - 0.015));
-     * 	ring.push(arc)
-     * 
-     *     } else {
-     * 	for (var i = 0 ; i < n_ring ; i++){
-     * 	    let delta = 0.6 / n_ring
-     * 	    let arc = d3.svg.arc()
-     * 			.startAngle(0)
-     * 			.endAngle(pi)
-     * 			.innerRadius(radius * ((0.3 + 0.015 + (i * delta))))
-     * 			.outerRadius(radius * ((0.3 - 0.015 + ((i+1) * delta))));
-     * 	    ring.push(arc)
-     * 	}
-     *     }*/
-} 
 
-function update_ring(){
-    rings[0].forEach(function(d){
-	d.remove();
-    });
-    rings[0].length = 0;
-    rings = rings.data(ring);
-    rings.enter()
-	.append("path")
-	.attr("class","ring")
-	.attr("d", function(d){
-	    return d()
-	});
-}
+
+
 function update_border_ring(){
     border_rings[0].forEach(function(d){
 	d.remove();
@@ -120,35 +109,105 @@ function update_extreme_ring(){
 }
 
 function update_rings(){
-    update_ring();
     update_border_ring();
     update_extreme_ring();
+    update_events();
 }
 
 function change_ring(){
+    set_ring();
     set_extreme_ring();
     set_border_ring();
-    set_ring();
     update_rings(); 
 }
 
 function inc_ring(){
     if (n_ring < 6){
 	n_ring++;
+	change_ring();
     }
-    change_ring();
 }
 
 function dec_ring(){
-    if (n_ring > 0){
+    if (n_ring > 1){
 	n_ring--;
+	change_ring();
     }
-    change_ring();
 }
 
 inc_ring();
 
 
+
+/*================*/
+
+function d_radius(){
+    return 0.01 * radius;
+}
+
+function create_event(start, end, lane){
+    if ((lane >= 0) && (lane < n_ring)){
+	let arc = {
+	    "arc": d3.svg.arc()
+		.startAngle(to_h(start/24))
+		.endAngle(to_h(end/24))
+		.innerRadius(ring[lane].inner * 1.02) 
+		.outerRadius(ring[lane].outer * 0.98),
+	    "start": start,
+	    "end": end,
+	    "lane": lane
+	};
+	return arc
+    }
+}
+
+function add_event(start, end, lane){
+    let arc = create_event(start, end, lane);
+    if (typeof arc != undefined){
+	event.push(arc)
+    }
+}
+
+
+function update_event(){
+    event = event.map(function(d){
+	return create_event(d.start, d.end, d.lane);
+    })
+}
+
+function add_events(){
+    events = events.data(event);
+    events.enter()
+	.append("path")
+	.attr("class","event")
+	.attr("d", function(d){
+	    return d.arc()
+	});
+}
+
+
+function update_events(){
+    update_event();
+    events[0].forEach(function(d){
+	d.remove();
+    });
+    events[0].length = 0;
+    events = events.data(event);
+    events.enter()
+	.append("path")
+	.attr("class","event")
+	.attr("d", function(d){
+	    return d.arc()
+	});
+}
+
+function add_e3(){
+    add_event(21,24,0); // core
+    add_event(h_n_m(4,10),h_n_m(4,30),0); // nap 1
+    add_event(h_n_m(8,10),h_n_m(8,30),0); // nap 2
+    add_event(h_n_m(14,40),15,0); // nap 3
+    add_events();
+}
 
 /* let arc = d3.svg.arc()
  * 	    .startAngle(0)
